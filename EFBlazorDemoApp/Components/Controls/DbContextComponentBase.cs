4ef703e82b0154d92b0e342187a96cc0513920c4
sonnemaf <fons.sonnemans@reflectionit.nl>
2024-03-20 16:25:25 +0100

    Add project files.

---
+++ EFBlazorDemoApp/Components/Controls/DbContextComponentBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFBlazorDemoApp.Components.Controls {
+    public abstract class DbContextComponentBase<T> : ComponentBase, IDisposable where T : DbContext {
+        private T? _dbContext;
+
+        [Inject]
+        public required IDbContextFactory<T> DatabaseFactory { get; init; }
+
+        public T DbContext => _dbContext ??= DatabaseFactory.CreateDbContext();
+
+        private bool _disposedValue;
+
+        protected virtual void Dispose(bool disposing) {
+            if (!_disposedValue) {
+                if (disposing) {
+                    _dbContext?.Dispose();
+                }
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose() {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ EFBlazorDemoApp/Program.cs
@@ -0,0 +1,34 @@
+global using EFBlazorDemoApp.Models;
+
+using EFBlazorDemoApp.Components;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddDbContext<LocalDBMSSQLLocalDBContext>(
+//        options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContextFactory<LocalDBMSSQLLocalDBContext>(
+        options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Add services to the container.
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>();
+
+app.Run();
